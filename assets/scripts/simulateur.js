ccb85388ffac40df5d33363ce2f202b6391a4c31
CorentinPolard <corentinpolard@gmail.com>
2025-08-19 11:36:30 +0200

    ajout de vérification des données dans le js

--- assets/scripts/simulateur.js
+++ assets/scripts/simulateur.js
@@ -18,16 +18,26 @@ function roundToTwoDecimals(n) {
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let netPrice = surface.value * frequency.value * basePrice;
+    const surfaceValue = parseFloat(surface.value);
+    const frequencyValue = parseInt(frequency.value);
 
-    if (windowOption.checked) {
-        netPrice *= 1.1;
+    if (isNaN(surfaceValue) || surfaceValue <= 0) {
+        resultParagraphe.innerHTML = "<span class='form-error'>Veuillez entrer une valeur valide pour la surface (>0).</span>";
+    } else if (isNaN(frequencyValue) || frequencyValue <= 0 || frequencyValue > 5) {
+        resultParagraphe.innerHTML = "<span class='form-error'>Veuillez entrer une valeur valide pour la fréquence.</span>";
+    } else {
+        let netPrice = surfaceValue * frequencyValue * basePrice;
+
+        if (windowOption.checked) {
+            netPrice *= 1.1;
+        }
+
+        netPrice = roundToTwoDecimals(netPrice);
+        const TVA = roundToTwoDecimals(netPrice * 0.2);
+        const totalPrice = netPrice + TVA;
+
+        resultParagraphe.innerHTML = `<b>Montant HT</b> = ${netPrice}€<br><b>TVA (20%)</b> = ${TVA}€<br><b>Montant TTC</b> = ${totalPrice}€`
     }
 
-    netPrice = roundToTwoDecimals(netPrice);
-    const TVA = roundToTwoDecimals(netPrice * 0.2);
-    const totalPrice = netPrice + TVA;
-
-    resultParagraphe.innerHTML = `<b>Montant HT</b> = ${netPrice}€<br><b>TVA (20%)</b> = ${TVA}€<br><b>Montant TTC</b> = ${totalPrice}€`
     resultContainer.classList.remove("hidden");
 })
